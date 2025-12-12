db5c1b56cf18a9eab81b57be43ac8c94f8212cdc
likith yeruva <yeruvalikith@gmail.com>
2025-12-12 00:22:26 +0000

    changes made to correlation analysis code

--- correlation_analysis.R
+++ correlation_analysis.R
@@ -1,6 +1,9 @@
 #  CORRELATION TEST BETWEEN MINUTES PLAYED (MP) AND POINTS SCORED (PTS)
+#Pearson(default)
 cor_result <- cor.test(df$MP, df$PTS)
+#Explicit pearson
 cor_pearson <- cor.test(df$MP, df$PTS, method = "pearson")
+#Spearmam
 cor_spearman <- cor.test(df$MP, df$PTS, method = "spearman")
 
 #  Print to console
