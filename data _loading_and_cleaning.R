589ffaf96e7ef06911479c31d6f99bd0a92fce0f
AnanthaLokesh <av25aap@herts.ac.uk>
2025-12-23 16:43:14 +0530

    Refactor data loading and cleaning script
    Removed unnecessary comments and cleaned up the code.

--- data _loading_and_cleaning.R
+++ data _loading_and_cleaning.R
@@ -1,12 +1,5 @@
-#Load libray
 library(readr)
-
-#  Load dataset
 df <- read_csv("nba_data_processed.csv")
-
-#  Cleaning columns MP and PTS to ensure they are numeric
 df$MP  <- as.numeric(df$MP)
 df$PTS <- as.numeric(df$PTS)
-
-# Remove rows that have missing MP or PTS
 df <- df[!is.na(df$MP) & !is.na(df$PTS), ]
